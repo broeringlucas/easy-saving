4101c6986be9b3984af8154309b9266321f39a2a
broeringlucas <lucas10santos15@gmail.com>
2025-08-01 05:31:40 -0300

    pf deus

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ const corsOptions = {
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["set-cookie"],
 };
 
 app.use(cors(corsOptions));
