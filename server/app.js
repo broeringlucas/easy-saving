faa6a35dba34426accb73e2b05af708661a9c6cf
broeringlucas <lucas10santos15@gmail.com>
2025-08-01 04:10:10 -0300

    client url

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const port = process.env.PORT;
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:8080",
+  origin: process.env.FRONTEND_URL || "http://localhost:8080",
   credentials: true,
 };
 
