01cf377873d70a3ee4781fbec00f9c57073a9912
broeringlucas <lucas10santos15@gmail.com>
2025-08-03 04:01:02 -0300

    change cookie config

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -56,7 +56,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "lax",
       path: "/",
       maxAge: 24 * 60 * 60 * 1000,
       domain: process.env.SERVER_URL,
